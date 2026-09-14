094b270d42bec413ca6505a7a7b8f63c3f8c1384
RamzoCodes <ramizkhurshid@gmail.com>
2026-09-14 11:32:19 +0200

    Starten av Kunskapskontroll 1 del B

---
+++ Kurser-Studerande/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+// System för kurser och studerande
+//en kurs har flera studerande
+//en student kan gå flera kurser
+//minst två klasser - student och course
+
+string Student;
+string Course;
+string Name;
